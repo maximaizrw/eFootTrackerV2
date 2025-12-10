9d389057d3d1272d869a130380baafb966a5bf1a
Maxi Maiz <maximaizrw@gmail.com>
2025-12-10 16:28:52 +0000

    eFootTrackerV2

---
+++ src/components/player-tester-dialog.tsx
@@ -0,0 +1,13 @@
+
+import React from 'react';
+
+const PlayerTesterDialog = () => {
+    return (
+        <div>
+            <h1>Player Tester</h1>
+            {/* Player selection and affinity display will go here */}
+        </div>
+    );
+};
+
+export default PlayerTesterDialog;
